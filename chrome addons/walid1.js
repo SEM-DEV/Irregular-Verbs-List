3865b1fcd0676b344ae218f3412d6e39231cd90f
SEM-DEV Inc <oualid909@gmail.com>
2021-01-01 00:17:22 +0300

    Add files via upload

---
+++ chrome addons/walid1.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+window.onload = function() {
+  var input = document.getElementById("myInput").focus();
+}
